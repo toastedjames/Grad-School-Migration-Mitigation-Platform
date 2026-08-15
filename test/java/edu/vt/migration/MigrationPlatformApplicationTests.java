f211ec1dc4436822d5b61ddb098f19d0b0a322d6
Somak Goswami <92353322+toastedjames@users.noreply.github.com>
2026-08-14 23:22:50 -0400

    Add files via upload

---
+++ test/java/edu/vt/migration/MigrationPlatformApplicationTests.java
@@ -0,0 +1,16 @@
+package edu.vt.migration;
+
+import org.junit.jupiter.api.Test;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+@SpringBootTest
+class MigrationPlatformApplicationTests {
+
+
+    @Test
+    void contextLoads() {
+
+    }
+}
